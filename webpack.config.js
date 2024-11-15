1692db2026942e7492feb1fbecd6df96c076675c
AReum <alliejj99@gmail.com>
2024-11-15 21:10:41 +0900

    create-react-app 없이 리액트 실행시키기

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/, // .js, .jsx 파일 처리
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'], // 확장자 생략 가능
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './dist/index.html', // HTML 템플릿 경로
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 8080,
+  },
+};
